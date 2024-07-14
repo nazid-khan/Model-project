5b90806b78344b244f1ffd528453199f853eb8e1
nazid <nazidk773@gmail.com>
2024-07-14 19:08:48 +0530

    add closebtn functionality

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const closeBtn = document.querySelector(".close-btn");
 
 modalBtn.addEventListener("click",function(){
     modal.classList.add("open-modal");
-    console.log(modal.classList);
 });
 
-
+closeBtn.addEventListener("click",function(){
+    modal.classList.remove("open-modal");
+});
